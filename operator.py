90a633a4ba0b1200822641751a6733242c6ccfbd
shruti-raiyani <shrutiraiyani004@gmail.com>
2026-01-27 09:27:32 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,53 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #arithmathic operator
+>>> a=23
+>>> b=10
+>>> print(a+b)
+33
+>>> print(a-b)
+13
+>>> print(a*b)
+230
+>>> print(a/b)
+2.3
+>>> #assignment operator
+>>> x=23
+>>> print(x)
+23
+>>> #unary operator
+>>> x=10
+>>> print(-x)
+-10
+>>> #relational operator
+>>> p=10
+>>> q=25
+>>> print(p<q)
+True
+>>> #logical operator
+>>> p=True
+>>> q=False
+>>> print(p)
+True
+>>> #boolean operator
+>>> is_pass=True
+>>> print(is_pass)
+True
+>>> #bitwise operator
+>>> a=5
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a|b)
+7
+>>> #membership operator
+>>> numbers=[1,2,3,4]
+>>> print(2 in numbers)
+True
+>>> 
+>>> #identity operator
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> 
