64e145620b8868a0963913858b6f8ccce29ad567
shruti-raiyani <shrutiraiyani004@gmail.com>
2026-01-31 07:59:26 +0530

    Add files via upload

---
+++ IO.py
@@ -0,0 +1,9 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello..!!")
+hello..!!
+>>> name=input("enter your name: ")
+enter your name: shruti
+>>> print("how are you?...",name)
+how are you?... shruti
+>>> 
