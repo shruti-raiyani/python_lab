d5a4d57acc53d8ee45b680e1518ec582be829412
shruti-raiyani <shrutiraiyani004@gmail.com>
2026-02-24 08:19:00 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,62 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> i=1
+>>> while i<10:
+	print(i)
+	i=i+1
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+>>> n=int(input("enter n:"))
+enter n:7
+>>> i=1
+>>> s=0
+>>> while i<=n:
+	s=s+1
+	i=i+1
+	print("sum=",s)
+
+	
+sum= 1
+sum= 2
+sum= 3
+sum= 4
+sum= 5
+sum= 6
+sum= 7
+>>> num=int(input("enter number:")
+	i=1
+	
+SyntaxError: invalid syntax
+>>> num=int(input("enter number:"))
+enter number:9
+>>> i=1
+>>> whilei<=10:
+	
+SyntaxError: invalid syntax
+>>> num=int(input("enter number:"))
+enter number:8
+>>> i=1
+>>> while i<10:
+	print(num,"x",i,"=",num*i)
+	i=i+1
+
+	
+8 x 1 = 8
+8 x 2 = 16
+8 x 3 = 24
+8 x 4 = 32
+8 x 5 = 40
+8 x 6 = 48
+8 x 7 = 56
+8 x 8 = 64
+8 x 9 = 72
+>>> 
