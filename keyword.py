834c9127ff2cc56942e23b8004593204f68c3297
shruti-raiyani <shrutiraiyani004@gmail.com>
2026-04-07 04:26:32 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,21 @@
+def Student_info(name, age, city):
+    print("name :", name)
+    print("age :", age)
+    print("city :", city)
+
+def display(a, b, c):
+    print("a =", a)
+    print("b =", b)
+    print("c =", c)
+    print("--- dispaly output---")
+display(1, c = 3, b = 2)  
+
+def Simple_interest(p, r, t):
+    si =(p * r * t) / 100
+    print("simple Interest :", si)
+    print("\n--- simple Interest output---")
+Simple_interest(p = 1000, t =2, r = 2.5) 
+Simple_interest(t = 2.5, p = 15000, r = 3)   
+
+    
+
